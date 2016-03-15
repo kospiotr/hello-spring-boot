792429be64550eb95977e1e78d24902967ab16cb
Piotr Kosmowski <kospiotr@gmail.com>
2016-03-15 22:55:43 +0100

    Init project again

---
+++ src/main/java/com/github/kospiotr/hsb/HelloSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.github.kospiotr.hsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBootApplication.class, args);
+	}
+}
